fd823acfe5465a0dc1ac2e18643bbfe3091a9562
AmreenDhillon <92673783+AmreenDhillon@users.noreply.github.com>
2023-03-05 17:53:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function setup(){
+    canvas=createCanvas(280,280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth= window.speechSynthesis;
+}
+
+function clearCanvas(){
+    background("white");
+}
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+       line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+    check_sketch();
+    if(drawn_sketch == sketch){
+        timer_counter = 0;
+        answer_holder = "set";
+        score = score+1;
+        document.getElementById("score").innerHTML = "Score: "+score;
+    }
+}function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML= 'Label:'+ results[0].label;
+
+    document.getElementById('confidence').innerHTML= 'Confidence:'+Math.round(results[0].confidence *100)+'%';
+
+    utterThis= new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
